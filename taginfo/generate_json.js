7ff75560086e2239f9be918c7232b44b5da83c92
Quinn <clear.tsai@gmail.com>
2024-02-08 09:56:35 +0000

    add taginfo.json for landuse

---
+++ taginfo/generate_json.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+
+function parseLine(line) {
+    let key = line.split(':')[0];
+    let values = line.split(':')[1];
+    if (values) {
+        values = values.split(',');
+        for (let i = 0; i < values.length; i++) {
+            values[i] = values[i].trim();
+        }
+    }
+    if (values == undefined) {
+        return [{"key": key}]
+    }
+    return values.map((value) => {
+        return {"key": key, "value": value}
+    });
+}
+
+fs.readFile('taglist.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    data = data.split('\n');
+    var tags = [];
+    for (let i = 0; i < data.length; i++) {
+        tags.push(...parseLine(data[i]));
+    }
+    // console.log(tags);
+    let output = {
+        "data_format": 1,
+        "data_updated": new Date(),
+        "project": {
+         "name" : "Tracestrack Topo Universal",
+         "description" : "A universal topo map for hikers, climbers, and mountaineers.",
+         "icon_url" : "https://www.tracestrack.com/img/logo_white.png",
+         "contact_name" : "Tracestrack",
+         "contact_email" : "info@tracestrack.com"
+        },
+        "tags" : tags
+    }
+    fs.writeFile('taginfo.json', JSON.stringify(output), (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log('File has been created');
+    });
+
+});
